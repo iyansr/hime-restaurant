1f67877e82eee616d741ac953a35c15ccc7c1859
IyanSR <ikhyan7@gmail.com>
2020-01-01 22:07:11 +0700

    Fallback Image

--- src/Components/Home/FoodItem.js
+++ src/Components/Home/FoodItem.js
@@ -1,10 +1,21 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Icon, Row, Col, Card, Skeleton, message } from 'antd'
+import {
+	Icon,
+	Row,
+	Col,
+	Card,
+	Skeleton,
+	message,
+	Button,
+	Popover,
+	Popconfirm,
+} from 'antd'
 import ContentLoader from 'react-content-loader'
 import styles from './style.module.css'
 import FoodContext from '../../Context/Food/foodContext'
 import convertToRupiah from '../../utils/rupiah'
 import CheckoutContext from '../../Context/Checkout/checkOutContext'
+import ReactImageFallback from 'react-image-fallback'
 
 const MyLoader = () => (
 	<ContentLoader
@@ -20,6 +31,9 @@ const MyLoader = () => (
 const FoodItem = () => {
 	const foodContext = useContext(FoodContext)
 	const checkoutContext = useContext(CheckoutContext)
+	const [imgerror, setImgerror] = useState(false)
+	const fallbackImage =
+		'https://cdn.dribbble.com/users/1012566/screenshots/4187820/topic-2.jpg'
 
 	const { foods, cart, addToCart, getFood, loading } = foodContext
 	const { changeCheckoutId, checkOutId } = checkoutContext
@@ -42,7 +56,8 @@ const FoodItem = () => {
 								<MyLoader />
 							) : (
 								<div className={styles.bg_image}>
-									<img
+									<ReactImageFallback
+										initialImage='https://media1.tenor.com/images/556e9ff845b7dd0c62dcdbbb00babb4b/tenor.gif'
 										onClick={() =>
 											!localStorage.getItem('@usertoken')
 												? message.error('You should login ')
@@ -56,6 +71,7 @@ const FoodItem = () => {
 														quantity: 1,
 												  })
 										}
+										fallbackImage='https://cdn.dribbble.com/users/1012566/screenshots/4187820/topic-2.jpg'
 										alt='example'
 										src={d.image}
 										width='100%'
@@ -66,6 +82,7 @@ const FoodItem = () => {
 												cart.filter(c => c.id === d.id).length > 0 ? 0.5 : 1,
 										}}
 									/>
+
 									{cart.filter(c => c.id === d.id).length > 0 && (
 										<div className={styles.image_overlay}>
 											<Icon
@@ -81,6 +98,33 @@ const FoodItem = () => {
 						<Skeleton loading={loading} paragraph={{ rows: 1 }} active>
 							<h4>{d.name}</h4>
 							<h3>{convertToRupiah(d.price)}</h3>
+							<Popover
+								placement='rightBottom'
+								trigger='click'
+								content={
+									<div>
+										<Button type='primary' size='small'>
+											Edit
+										</Button>
+
+										<Popconfirm
+											// placement='rightBottom'
+											title='Are you sure want to delete?'
+											okText='Yes'
+											cancelText='No'>
+											<Button
+												type='danger'
+												size='small'
+												style={{ marginLeft: 5 }}>
+												Delete
+											</Button>
+										</Popconfirm>
+									</div>
+								}>
+								<Button type='dashed' size='small' style={{ float: 'right' }}>
+									Action
+								</Button>
+							</Popover>
 						</Skeleton>
 					</Card>
 				</Col>
